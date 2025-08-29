89d87323041389dc090dace768e3331808c95a27
Emily Soth <43770515+emlys@users.noreply.github.com>
2025-08-29 00:13:35 +0000

    Add validate endpoint and basic tests (#4)
    * add validate process to validate an invest datastack
    
    * add basic tests for execute and validate endpoints
    
    * clean up
    
    * create github actions workflow to run tests on push and PR
    
    * install pytest before testing
    
    * configure pygeoapi server before running tests
    
    * minor cleanup of unit tests
    
    * change test data rasters to int8 and LZW compress

---
+++ tests/test_server.py
@@ -0,0 +1,52 @@
+import json
+import os.path
+import unittest
+
+from pygeoapi import flask_app
+
+
+class PyGeoAPIServerTests(unittest.TestCase):
+
+    def setUp(self):
+        self.client = flask_app.APP.test_client()
+        self.raster_path = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), 'test_data/carbon_willamette.invs.json'))
+
+    def tearDown(self):
+        pass
+
+    def testExecuteProcessMetadata(self):
+        response = self.client.get(f'/processes/execute')
+        self.assertEqual(response.status_code, 200)
+
+    def testExecuteProcessExecution(self):
+        response = self.client.post(f'/processes/execute/execution', json={
+            'inputs': {
+                'datastack_path': self.raster_path
+            }
+        })
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertTrue(os.path.exists(data['workspace_dir']))
+
+    def testValidateProcessMetadata(self):
+        response = self.client.get(f'/processes/validate')
+        self.assertEqual(response.status_code, 200)
+
+    def testValidateProcessExecution(self):
+        """Validation of a datastack should return validation messages"""
+        response = self.client.post(f'/processes/validate/execution', json={
+            'inputs': {
+                'datastack_path': self.raster_path
+            }
+        })
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(data, {
+            'validation_errors': [
+                {
+                    'input_ids': ['workspace_dir'],
+                    'error_message': 'Key is missing from the args dict'
+                }
+            ]
+        })
