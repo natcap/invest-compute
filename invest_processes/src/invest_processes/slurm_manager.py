9ffd74589e9a3f9be055f15245efbb41cdc0f386
Emily Soth <esoth@stanford.edu>
2026-01-07 14:41:36 -0800

    debugging

--- invest_processes/src/invest_processes/slurm_manager.py
+++ invest_processes/src/invest_processes/slurm_manager.py
@@ -156,7 +156,7 @@ class SlurmManager(BaseManager):
         :returns: `dict` of job result
         """
         print(subprocess.run([
-            'sacct', '-o', 'JobID,Comment'
+            'sacct', '-o', 'JobID,Comment%0'
         ], capture_output=True, text=True, check=True).stdout.strip())
 
         comment = subprocess.run([
