00dbf5ea5a07b7ba2133677eab89ab9a572a44e4
Emily Soth <esoth@stanford.edu>
2026-01-12 14:20:36 -0800

    debugging

--- tests/test_server.py
+++ tests/test_server.py
@@ -113,41 +113,41 @@ class PyGeoAPIServerTests(unittest.TestCase):
             }
         )
 
-    # def testExecuteProcessErrorSync(self):
-    #     """Test executing a datastack that should cause a model error."""
-    #     response = self.client.post(f'/processes/execute/execution', json={
-    #         'inputs': {
-    #             # this datastack includes an invalid raster path
-    #             'datastack_url': 'https://github.com/natcap/invest-compute/raw/refs/heads/feature/compute-note-playbook/tests/test_data/invest_carbon_error_datastack.tgz'
-    #         }
-    #     })
-    #     self.assertEqual(response.status_code, 400)
-    #     data = json.loads(response.get_data(as_text=True))
-    #     self.assertEqual(set(data.keys()), {'workspace_url'})
+    def testExecuteProcessErrorSync(self):
+        """Test executing a datastack that should cause a model error."""
+        response = self.client.post(f'/processes/execute/execution', json={
+            'inputs': {
+                # this datastack includes an invalid raster path
+                'datastack_url': 'https://github.com/natcap/invest-compute/raw/refs/heads/feature/compute-note-playbook/tests/test_data/invest_carbon_error_datastack.tgz'
+            }
+        })
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(set(data.keys()), {'workspace_url'})
 
-    #     local_dest_path = os.path.join(self.workspace_dir, 'results')
-    #     os.mkdir(local_dest_path)
-    #     subprocess.run([
-    #         'gcloud', 'storage', 'cp', '--recursive', f'{data["workspace_url"]}/*', local_dest_path
-    #     ], check=True)
-    #     self.assertEqual(
-    #         set(os.listdir(local_dest_path)),
-    #         {
-    #             'datastack.tgz',     # datastack archive downloaded from the input url
-    #             'datastack',         # extracted datastack directory
-    #             'stdout.log',        # stdout from the slurm job
-    #             'stderr.log',        # stderr from the slurm job
-    #             'script.slurm',      # the slurm script sent to sbatch
-    #             'carbon_workspace',  # the invest model workspace directory
-    #             'results.json'       # json results file used by pygeoapi
-    #         }
-    #     )
+        local_dest_path = os.path.join(self.workspace_dir, 'results')
+        os.mkdir(local_dest_path)
+        subprocess.run([
+            'gcloud', 'storage', 'cp', '--recursive', f'{data["workspace_url"]}/*', local_dest_path
+        ], check=True)
+        self.assertEqual(
+            set(os.listdir(local_dest_path)),
+            {
+                'datastack.tgz',     # datastack archive downloaded from the input url
+                'datastack',         # extracted datastack directory
+                'stdout.log',        # stdout from the slurm job
+                'stderr.log',        # stderr from the slurm job
+                'script.slurm',      # the slurm script sent to sbatch
+                'carbon_workspace',  # the invest model workspace directory
+                'results.json'       # json results file used by pygeoapi
+            }
+        )
 
-    #     # expect model error to be captured in stderr.log
-    #     with open(os.path.join(local_dest_path, 'stderr.log')) as err_log:
-    #         self.assertIn(
-    #             'RuntimeError: does_not_exist.tif: No such file or directory',
-    #             err_log.read())
+        # expect model error to be captured in stderr.log
+        with open(os.path.join(local_dest_path, 'stderr.log')) as err_log:
+            self.assertIn(
+                'RuntimeError: does_not_exist.tif: No such file or directory',
+                err_log.read())
 
     def testValidateProcessMetadata(self):
         response = self.client.get(f'/processes/validate')
