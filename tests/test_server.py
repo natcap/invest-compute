cdf01ce9ff5e83d494e7a5db6eb67d5cae8e34d3
Emily Soth <esoth@stanford.edu>
2026-01-12 08:55:50 -0800

    debugging

--- tests/test_server.py
+++ tests/test_server.py
@@ -24,29 +24,27 @@ class PyGeoAPIServerTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def testExecuteProcessExecutionSync(self):
+        """Test execution of the 'execute' process in sync mode."""
         response = self.client.post('/processes/execute/execution', json={
             'inputs': {
                 'datastack_url': self.datastack_url
             }
         })
-        print(response.headers)
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.get_data(as_text=True))
+        execution_response = json.loads(response.get_data(as_text=True))
         # in sync mode with default response type ("raw"), the process
         # outputs should be returned directly in the json response
-        self.assertEqual(set(data.keys()), {'workspace_url'})
-        # self.assertEqual(response.headers['Location'], f'http://localhost:5000/jobs/{data["job_id"]}')
+        self.assertEqual(set(execution_response.keys()), {'workspace_url'})
 
-        job_url = response.headers['Location'].split('http://localhost:5000')[1]
-        response = json.loads(self.client.get(job_url).get_data(as_text=True))
-        self.assertEqual(response['status'], 'successful')
+        job_url = execution_response.headers['Location'].split('http://localhost:5000')[1]
+        job_response = json.loads(self.client.get(job_url).get_data(as_text=True))
+        self.assertEqual(job_response['status'], 'successful')
 
         results_endpoint_response = json.loads(self.client.get(
             f'{job_url}/results?f=json').get_data(as_text=True))
-        print('response from results endpoint:', response)
-
-        self.assertEqual(data, results_endpoint_response)
-
+        # in sync mode, the same results should be returned from the initial
+        # execution endpoint and from any subsequent calls to the results endpoint
+        self.assertEqual(execution_response, results_endpoint_response)
 
         local_dest_path = os.path.join(self.workspace_dir, 'results')
         os.mkdir(local_dest_path)
@@ -69,35 +67,39 @@ class PyGeoAPIServerTests(unittest.TestCase):
         # curl -X POST -H "Content-Type: application/json" -d '{"inputs": {"datastack_url": "https://github.com/natcap/invest-compute/raw/refs/heads/feature/compute-note-playbook/tests/test_data/invest_carbon_datastack.tgz"}}' localhost:5000/processes/execute/execution
 
     def testExecuteProcessExecutionAsync(self):
+        """Test execution of the 'execute' process in async mode."""
         response = self.client.post(f'/processes/execute/execution',
             json={'inputs': {'datastack_url': self.datastack_url}},
             headers={'Prefer': 'respond-async'})
         print(response.headers)
         self.assertEqual(response.status_code, 201)
-        data = json.loads(response.get_data(as_text=True))
+        execution_response = json.loads(response.get_data(as_text=True))
         # pygeoapi incorrectly calls this key 'id' instead of 'job_id'
         # https://github.com/geopython/pygeoapi/issues/2197
-        self.assertEqual(set(data.keys()), {'status', 'type', 'id'})
-        self.assertEqual(data['status'], 'accepted')
-        self.assertEqual(data['type'], 'process')  # according to the OGC standard this should always be 'process'
-        self.assertEqual(response.headers['Location'], f'http://localhost:5000/jobs/{data["id"]}')
+        self.assertEqual(set(execution_response.keys()), {'status', 'type', 'id'})
+        self.assertEqual(execution_response['status'], 'accepted')
+        self.assertEqual(execution_response['type'], 'process')  # according to the OGC standard this should always be 'process'
+        self.assertEqual(
+            response.headers['Location'],
+            f'http://localhost:5000/jobs/{execution_response["id"]}')
 
         # poll status until the job finishes
         # TODO: test with a longer running job
         while True:
-            response = json.loads(self.client.get(
+            job_response = json.loads(self.client.get(
                 f'/jobs/{data["id"]}').get_data(as_text=True))
-            print('status:', response['status'])
-            self.assertNotIn(response['status'], {'failed', 'dismissed'})
+            print('status:', job_response['status'])
+            self.assertNotIn(job_response['status'], {'failed', 'dismissed'})
             if response['status'] == 'successful':
                 break
 
-        response = json.loads(self.client.get(
+        results_response = json.loads(self.client.get(
             f'/jobs/{data["id"]}/results?f=json').get_data(as_text=True))
+        print('results response:', results_response)
         local_dest_path = os.path.join(self.workspace_dir, 'results')
         os.mkdir(local_dest_path)
         subprocess.run([
-            'gcloud', 'storage', 'cp', '--recursive', f'{response['results']}/*', local_dest_path
+            'gcloud', 'storage', 'cp', '--recursive', f'{results_response['results']}/*', local_dest_path
         ], check=True)
         self.assertEqual(
             set(os.listdir(local_dest_path)),
