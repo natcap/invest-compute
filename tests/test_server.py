da8b25d5bb4ce352918bd47a161caef27a34b788
Emily Soth <esoth@stanford.edu>
2026-01-22 11:34:05 -0800

    debugging

--- tests/test_server.py
+++ tests/test_server.py
@@ -170,103 +170,103 @@ class PyGeoAPIServerTests(unittest.TestCase):
             }
         )
 
-    def testExecuteProcessErrorSync(self):
-        """Test executing a datastack that should cause a model error."""
-        response = self.client.post(
-            '/processes/execute/execution',
-            json={'inputs': {'datastack_url': ERROR_DATASTACK_URL}}
-        )
-        print(response)
-        data = json.loads(response.get_data(as_text=True))
-        print(data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(set(data.keys()), {'workspace_url'})
+#     def testExecuteProcessErrorSync(self):
+#         """Test executing a datastack that should cause a model error."""
+#         response = self.client.post(
+#             '/processes/execute/execution',
+#             json={'inputs': {'datastack_url': ERROR_DATASTACK_URL}}
+#         )
+#         print(response)
+#         data = json.loads(response.get_data(as_text=True))
+#         print(data)
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(set(data.keys()), {'workspace_url'})
 
-        local_dest_path = os.path.join(self.workspace_dir, 'results')
-        os.mkdir(local_dest_path)
-        subprocess.run([
-            'gcloud', 'storage', 'cp', '--recursive',
-            f'{data["workspace_url"]}/*', local_dest_path
-        ], check=True)
-        print('stdout:')
-        with open(os.path.join(local_dest_path, 'stdout.log')) as f:
-            print(f.read())
+#         local_dest_path = os.path.join(self.workspace_dir, 'results')
+#         os.mkdir(local_dest_path)
+#         subprocess.run([
+#             'gcloud', 'storage', 'cp', '--recursive',
+#             f'{data["workspace_url"]}/*', local_dest_path
+#         ], check=True)
+#         print('stdout:')
+#         with open(os.path.join(local_dest_path, 'stdout.log')) as f:
+#             print(f.read())
 
-        print('stderr:')
-        with open(os.path.join(local_dest_path, 'stderr.log')) as f:
-            print(f.read())
+#         print('stderr:')
+#         with open(os.path.join(local_dest_path, 'stderr.log')) as f:
+#             print(f.read())
 
-        self.assertEqual(
-            set(os.listdir(local_dest_path)),
-            {
-                'datastack',         # extracted datastack directory
-                'stdout.log',        # stdout from the slurm job
-                'stderr.log',        # stderr from the slurm job
-                'script.slurm',      # the slurm script sent to sbatch
-                'carbon_workspace',  # the invest model workspace directory
-                'results.json'       # json results file used by pygeoapi
-            }
-        )
+#         self.assertEqual(
+#             set(os.listdir(local_dest_path)),
+#             {
+#                 'datastack',         # extracted datastack directory
+#                 'stdout.log',        # stdout from the slurm job
+#                 'stderr.log',        # stderr from the slurm job
+#                 'script.slurm',      # the slurm script sent to sbatch
+#                 'carbon_workspace',  # the invest model workspace directory
+#                 'results.json'       # json results file used by pygeoapi
+#             }
+#         )
 
-        # expect model error to be captured in stderr.log
-        with open(os.path.join(local_dest_path, 'stderr.log')) as err_log:
-            self.assertIn(
-                'RuntimeError: does_not_exist.tif: No such file or directory',
-                err_log.read())
+#         # expect model error to be captured in stderr.log
+#         with open(os.path.join(local_dest_path, 'stderr.log')) as err_log:
+#             self.assertIn(
+#                 'RuntimeError: does_not_exist.tif: No such file or directory',
+#                 err_log.read())
 
-    def testValidateProcessMetadata(self):
-        response = self.client.get('/processes/validate')
-        self.assertEqual(response.status_code, 200)
+#     def testValidateProcessMetadata(self):
+#         response = self.client.get('/processes/validate')
+#         self.assertEqual(response.status_code, 200)
 
-    def testValidateProcessExecutionSync(self):
-        """Validation of a datastack should return validation messages"""
-        response = self.client.post('/processes/validate/execution', json={
-            'inputs': {
-                'datastack_url': CARBON_DATASTACK_URL
-            }
-        })
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.get_data(as_text=True))
-        self.assertEqual(set(data.keys()),
-                         {'workspace_url', 'validation_results'})
-        self.assertEqual(
-            data['validation_results'],
-            [{
-                'input_ids': ['workspace_dir'],
-                'error_message': 'Key is missing from the args dict'
-            }]
-        )
+#     def testValidateProcessExecutionSync(self):
+#         """Validation of a datastack should return validation messages"""
+#         response = self.client.post('/processes/validate/execution', json={
+#             'inputs': {
+#                 'datastack_url': CARBON_DATASTACK_URL
+#             }
+#         })
+#         self.assertEqual(response.status_code, 200)
+#         data = json.loads(response.get_data(as_text=True))
+#         self.assertEqual(set(data.keys()),
+#                          {'workspace_url', 'validation_results'})
+#         self.assertEqual(
+#             data['validation_results'],
+#             [{
+#                 'input_ids': ['workspace_dir'],
+#                 'error_message': 'Key is missing from the args dict'
+#             }]
+#         )
 
-        local_dest_path = os.path.join(self.workspace_dir, 'results')
-        os.mkdir(local_dest_path)
-        subprocess.run([
-            'gcloud', 'storage', 'cp', '--recursive',
-            f'{data["workspace_url"]}/*', local_dest_path
-        ], check=True)
-        self.assertEqual(
-            set(os.listdir(local_dest_path)),
-            {
-                'datastack',         # extracted datastack directory
-                'stdout.log',        # stdout from the slurm job
-                'stderr.log',        # stderr from the slurm job
-                'script.slurm',      # the slurm script sent to sbatch
-                'results.json'       # json results file used by pygeoapi
-            }
-        )
+#         local_dest_path = os.path.join(self.workspace_dir, 'results')
+#         os.mkdir(local_dest_path)
+#         subprocess.run([
+#             'gcloud', 'storage', 'cp', '--recursive',
+#             f'{data["workspace_url"]}/*', local_dest_path
+#         ], check=True)
+#         self.assertEqual(
+#             set(os.listdir(local_dest_path)),
+#             {
+#                 'datastack',         # extracted datastack directory
+#                 'stdout.log',        # stdout from the slurm job
+#                 'stderr.log',        # stderr from the slurm job
+#                 'script.slurm',      # the slurm script sent to sbatch
+#                 'results.json'       # json results file used by pygeoapi
+#             }
+#         )
 
 
-class UtilsTests(unittest.TestCase):
+# class UtilsTests(unittest.TestCase):
 
-    def setUp(self):
-        self.workspace_dir = tempfile.mkdtemp()
+#     def setUp(self):
+#         self.workspace_dir = tempfile.mkdtemp()
 
-    def tearDown(self):
-        shutil.rmtree(self.workspace_dir)
+#     def tearDown(self):
+#         shutil.rmtree(self.workspace_dir)
 
-    def testDownloadAndExtractDatastack(self):
-        """Test utility function for downloading and extracting a datastack."""
-        download_and_extract_datastack(CARBON_DATASTACK_URL, self.workspace_dir)
-        self.assertEqual(
-            set(os.listdir(self.workspace_dir)),
-            {'data', 'log.txt', 'parameters.invest.json'}
-        )
+#     def testDownloadAndExtractDatastack(self):
+#         """Test utility function for downloading and extracting a datastack."""
+#         download_and_extract_datastack(CARBON_DATASTACK_URL, self.workspace_dir)
+#         self.assertEqual(
+#             set(os.listdir(self.workspace_dir)),
+#             {'data', 'log.txt', 'parameters.invest.json'}
+#         )
