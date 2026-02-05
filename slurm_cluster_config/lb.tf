ee08c730622e65fa8f485b17e1955fed2c17eae9
Emily Soth <esoth@stanford.edu>
2026-02-05 12:13:41 -0800

    clean up terraform file and add comments

--- slurm_cluster_config/lb.tf
+++ slurm_cluster_config/lb.tf
@@ -1,46 +1,51 @@
-# Enable the necessary APIs
-resource "google_project_service" "enable_cloud_resource_manager_api" {
-  service = "cloudresourcemanager.googleapis.com"
+# Enable the necessary APIs ---------------------------------------------------
+resource "google_project_service" "enable_services" {
+  project = var.project_id
+  for_each = toset([
+    "apigateway.googleapis.com",
+    "apikeys.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "run.googleapis.com",
+    "secretmanager.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "servicecontrol.googleapis.com"
+  ])
+  service            = each.key
+  disable_on_destroy = false
 }
 
-resource "google_project_service" "enable_secret_manager_api" {
-  service = "secretmanager.googleapis.com"
+
+# Cloud Run Service -----------------------------------------------------------
+#
+# This service runs a minimal nginx proxy that redirects traffic to the
+# internal server. This is necessary because the server is running privately
+# in the VPC network, but the API Gateway is outside the VPC network and needs
+# a public URL to point to. Cloud Run provides that public URL while also
+# having access to the VPC network and the private server running there.
+
+# Create a Service Account for the Cloud Run Service
+resource "google_service_account" "cloud_run_sa" {
+  account_id   = "cloud-run-sa"
+  display_name = "Service Account for Cloud Run"
 }
 
-resource "google_project_service" "enable_cloud_run_admin_api" {
-  service = "run.googleapis.com"
-}
-
-# Cloud Run -----------------------------------------------------
-
-# Create a Service Account for the API Gateway
-resource "google_service_account" "gateway_sa" {
-  account_id   = "api-gateway-sa"
-  display_name = "Service Account for API Gateway"
-}
-
-# Create a Service Account for the Bridge
-resource "google_service_account" "bridge_sa" {
-  account_id   = "bridge-proxy-sa"
-  display_name = "Cloud Run Bridge Service Account"
-}
-
-# Grant it access to the Nginx Secret
-resource "google_secret_manager_secret_iam_member" "bridge_secret_access" {
+# Grant it access to the nginx Secret
+resource "google_secret_manager_secret_iam_member" "cloud_run_secret_access" {
   project   = var.project_id
   secret_id = google_secret_manager_secret.nginx_config.secret_id
   role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${google_service_account.bridge_sa.email}"
+  member    = "serviceAccount:${google_service_account.cloud_run_sa.email}"
 }
 
-# Create the Cloud Run Service (The Bridge)
-resource "google_cloud_run_v2_service" "bridge" {
-  name     = "bridge-proxy"
+# Create the Cloud Run Service
+resource "google_cloud_run_v2_service" "proxy" {
+  name     = "cloud-run-proxy"
   location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL" # Accessible from Gateway (Public)
+  deletion_protection = false
 
   template {
-    service_account = google_service_account.bridge_sa.email
+    service_account = google_service_account.cloud_run_sa.email
 
     containers {
       image = "nginx:alpine" # Lightweight proxy
@@ -52,7 +57,7 @@ resource "google_cloud_run_v2_service" "bridge" {
         value = "8080"
       }
 
-      # We mount the config file (defined below)
+      # Mount the config file (defined below)
       volume_mounts {
         name       = "nginx-conf"
         mount_path = "/etc/nginx/conf.d"
@@ -65,7 +70,7 @@ resource "google_cloud_run_v2_service" "bridge" {
       force-update-key = google_secret_manager_secret_version.nginx_config_data.name
     }
 
-    # MOUNT THE CONFIG
+    # Mount the config
     volumes {
       name = "nginx-conf"
       secret {
@@ -77,7 +82,7 @@ resource "google_cloud_run_v2_service" "bridge" {
       }
     }
 
-    # CRITICAL: Enable Direct VPC Egress to reach Internal LB
+    # Enable VPC egress so that this service can reach the internal server
     vpc_access {
       network_interfaces {
         network    = "hpc-slurm-net"
@@ -88,9 +93,9 @@ resource "google_cloud_run_v2_service" "bridge" {
   }
 }
 
-# 3. Define the Nginx Config (Store as Secret for simplicity in TF)
+# 3. Define the Nginx Config (tore as Secret for simplicity in TF)
 resource "google_secret_manager_secret" "nginx_config" {
-  secret_id = "bridge-nginx-config"
+  secret_id = "proxy-nginx-config"
   replication {
     auto {}
   }
@@ -99,12 +104,11 @@ resource "google_secret_manager_secret" "nginx_config" {
 resource "google_secret_manager_secret_version" "nginx_config_data" {
   secret = google_secret_manager_secret.nginx_config.id
 
-  # This config listens on 8080 and proxies to your INTERNAL LB IP
+  # This config listens on 8080 and proxies to the internal server
   secret_data = <<EOF
 server {
     listen 8080;
     location / {
-        # Replace this with your Internal LB IP or DNS
         proxy_pass http://10.0.0.3:5000/;
     }
 }
@@ -116,7 +120,7 @@ EOF
 resource "google_cloud_run_v2_service_iam_binding" "invoker" {
   project  = var.project_id
   location = var.region
-  name     = google_cloud_run_v2_service.bridge.name
+  name     = google_cloud_run_v2_service.proxy.name
   role     = "roles/run.invoker"
 
   members = [
@@ -125,25 +129,13 @@ resource "google_cloud_run_v2_service_iam_binding" "invoker" {
 }
 
 
-#############################################################################
-
-resource "google_project_service" "gateway_services" {
-  project = var.project_id
-  for_each = toset([
-    "apigateway.googleapis.com",
-    "servicemanagement.googleapis.com",
-    "servicecontrol.googleapis.com",
-    "apikeys.googleapis.com"
-  ])
-  service            = each.key
-  disable_on_destroy = false
-}
-
-# -------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Serverless Network Endpoint Group
-# this allows the Load Balancer to talk to the API Gateway
+#
+# this is needed to allow the Load Balancer to talk to the API Gateway.
+#
 
-# 1. Serverless NEG (The Bridge)
+# Create the Network Endpoint Group
 resource "google_compute_region_network_endpoint_group" "gateway_neg" {
   provider = google-beta
   name                  = "gateway-neg"
@@ -156,7 +148,7 @@ resource "google_compute_region_network_endpoint_group" "gateway_neg" {
   }
 }
 
-# 2. Backend Service for the Gateway
+# Create the Backend Service
 resource "google_compute_backend_service" "gateway_backend" {
   name        = "gateway-backend-service"
   protocol    = "HTTPS" # Gateways are always HTTPS
@@ -168,16 +160,23 @@ resource "google_compute_backend_service" "gateway_backend" {
 }
 
 # -----------------------------------------------------------------------
-# API Gateway #######################################################
+# API Gateway
 
-# 1. Define the API Resource
+# Create a Service Account for the API Gateway
+resource "google_service_account" "gateway_sa" {
+  account_id   = "api-gateway-sa"
+  display_name = "Service Account for API Gateway"
+}
+
+# Create the API
 resource "google_api_gateway_api" "api" {
   provider = google-beta
   api_id   = "my-secure-api"
   project  = var.project_id
 }
 
-# 3. Create the Gateway Config
+# Create the API Config
+# this references the openapi yml, which defines our endpoints
 resource "google_api_gateway_api_config" "api_cfg" {
   provider      = google-beta
   api           = google_api_gateway_api.api.api_id
@@ -189,7 +188,7 @@ resource "google_api_gateway_api_config" "api_cfg" {
       contents = base64encode(
         templatefile(
           "../../bundled-openapi.yml",
-          { backend_url = google_cloud_run_v2_service.bridge.uri }
+          { backend_url = google_cloud_run_v2_service.proxy.uri }
         )
       )
     }
@@ -217,7 +216,7 @@ resource "google_project_service" "api_gateway_service" {
   disable_on_destroy = false
 }
 
-# 4. Deploy the Gateway
+# Create the API Gateway
 resource "google_api_gateway_gateway" "api_gw" {
   provider   = google-beta
   api_config = google_api_gateway_api_config.api_cfg.id
@@ -226,8 +225,15 @@ resource "google_api_gateway_gateway" "api_gw" {
 }
 
 
-# ----------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # API Key
+#
+# The client must provide this key in their requests as a URL parameter
+# called "key". The API Gateway enforces that the key is provided correctly.
+# The key may be accessed by running `terraform output api_key` after a
+# successful `terraform apply`.
+#
+# TODO: Generate separate keys for multiple users
 
 resource "google_apikeys_key" "primary" {
   name         = "primary-api-key"
@@ -248,24 +254,41 @@ output "api_key" {
 
 
 # ------------------------------------------------------------------------------
-# 1. GENERATE SELF-SIGNED CERTIFICATE (The "No Domain" Workaround)
-# ------------------------------------------------------------------------------
+# Load Balancer
+#
+# This is the client-facing component of the infrastructure. Clients will make
+# requests directly to the Load Balancer's IP address.
+# Benefits of using a Load Balancer here:
+# - It is a global service, so worldwide traffic is efficiently directed to the
+#   backend (vs. with a regional component like API Gateway, there is higher
+#   latency for clients farther from our zone e.g. us-central1-a)
+# - TODO: we can attach Cloud Armor to the Load Balancer
 
+# URL Map - Routes incoming requests to the Backend Service
+# "load-balancer-url-map" appears as a "Classic Application Load Balancer"
+# in the console
+resource "google_compute_url_map" "default" {
+  name            = "load-balancer-url-map"
+  default_service = google_compute_backend_service.gateway_backend.id
+}
+
+# Create a private key for the self-signed certificate
 resource "tls_private_key" "default" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
+# Create a self-signed certificate
 resource "tls_self_signed_cert" "default" {
   private_key_pem = tls_private_key.default.private_key_pem
 
-  # This subject is "fake" but required. It won't match your IP, causing the warning.
+  # This subject is "fake" but required. It won't match your IP, causing a warning.
   subject {
-    common_name  = "iap-protected-app.internal"
+    common_name  = "protected-app.internal"
     organization = "My Organization"
   }
 
-  validity_period_hours = 8760 # 1 Year
+  validity_period_hours = 8760 # 1 year
 
   allowed_uses = [
     "key_encipherment",
@@ -274,7 +297,7 @@ resource "tls_self_signed_cert" "default" {
   ]
 }
 
-# Upload the generated cert to Google Compute Engine
+# Upload the self-signed certificate to Google Compute Engine
 resource "google_compute_ssl_certificate" "default" {
   name_prefix = "self-signed-cert-"
   private_key = tls_private_key.default.private_key_pem
@@ -285,135 +308,22 @@ resource "google_compute_ssl_certificate" "default" {
   }
 }
 
-# ------------------------------------------------------------------------------
-# 2. UNMANAGED INSTANCE GROUP
-#    Collects the individual instances into a group the LB can target.
-# ------------------------------------------------------------------------------
-
-# variable "iap_client_name" {
-#   description = "IAP client name"
-#   type        = string
-# }
-
-# variable "iap_client_secret" {
-#   description = "IAP client secret"
-#   type        = string
-# }
-
-
-resource "google_compute_instance_group" "unmanaged_group" {
-  name        = "my-unmanaged-ig"
-  description = "Unmanaged instance group for Terraform LB"
-  network = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/global/networks/hpc-slurm-net"
-  instances   = [
-    "https://www.googleapis.com/compute/v1/projects/${var.project_id}/zones/${var.zone}/instances/hpcslurm-slurm-login-001"]
-
-  # CRITICAL: The Load Balancer looks for this specific named port
-  named_port {
-    name = "http"
-    port = 5000
-  }
-
-  zone = var.zone
-}
-
-# ------------------------------------------------------------------------------
-# 3. LOAD BALANCER COMPONENTS
-# ------------------------------------------------------------------------------
-
-# # Health Check - Monitors if instances are responsive
-# resource "google_compute_health_check" "default" {
-#   name               = "http-health-check"
-#   timeout_sec        = 1
-#   check_interval_sec = 1
-
-#   http_health_check {
-#     port = 5000
-#   }
-# }
-
-# URL Map - Routes incoming requests to the Backend Service
-# "http-lb-url-map" appears as a "Classic Application Load Balancer"
-# in the console
-#
-# TODO: use HTTPS
-resource "google_compute_url_map" "default" {
-  name            = "http-lb-url-map"
-  default_service = google_compute_backend_service.gateway_backend.id
-}
-
-# # Target Proxy - Termination point for HTTP connections
-# resource "google_compute_target_http_proxy" "default" {
-#   name    = "http-lb-proxy"
-#   url_map = google_compute_url_map.default.id
-# }
-
-# # Global Forwarding Rule - The Frontend IP Address
-# resource "google_compute_global_forwarding_rule" "default" {
-#   name       = "http-lb-forwarding-rule"
-#   target     = google_compute_target_http_proxy.default.id
-#   port_range = "80"
-# }
-
-# HTTPS Proxy - Uses the Self-Signed Cert
+# Create HTTPS Proxy using the self-signed certificate
 resource "google_compute_target_https_proxy" "default" {
-  name             = "iap-https-proxy"
+  name             = "https-proxy"
   url_map          = google_compute_url_map.default.id
   ssl_certificates = [google_compute_ssl_certificate.default.id]
 }
 
-# Forwarding Rule - Listens on HTTPS (443)
+# Create a Forwarding Rule - Listens on HTTPS (443)
 resource "google_compute_global_forwarding_rule" "default" {
-  name       = "iap-forwarding-rule"
+  name       = "forwarding-rule"
   target     = google_compute_target_https_proxy.default.id
   port_range = "443"
 }
 
-# ------------------------------------------------------------------------------
-# 4. FIREWALL RULES
-#    Required for the Google Load Balancer to talk to your instances.
-# ------------------------------------------------------------------------------
-
-# resource "google_compute_firewall" "allow_lb_health_check" {
-#   name    = "allow-lb-health-check"
-#   network = "hpc-slurm-net"
-
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["5000"]
-#   }
-
-#   # These are the specific IP ranges Google uses for health checks
-#   source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-#   target_tags   = ["http-server"]
-# }
-
-
-# IAP #####################################
-
-# resource "google_project_service" "iap_service" {
-#   project = var.project_id
-#   service = "iap.googleapis.com"
-#   disable_on_destroy = false
-# }
-
-# resource "google_iap_web_backend_service_iam_member" "iap_access" {
-#   project = var.project_id
-#   web_backend_service = google_compute_backend_service.default.name
-
-#   # Role required to access the app
-#   role = "roles/iap.httpsResourceAccessor"
-
-#   # The user you want to allow (change to your email)
-#   member = "user:esoth@stanford.edu"
-# }
-
-
 # TODO:
 # reserve a static external IP address for the load balancer
-
-
-
 
 output "load_balancer_ip" {
   description = "The public IP address of the global load balancer"
