dc7d9abeec5f641ceffb19ed97eff3bbdab30e11
Emily Soth <esoth@stanford.edu>
2026-04-27 10:43:05 -0700

    debugging

--- tests/test_server.py
+++ tests/test_server.py
@@ -56,6 +56,13 @@ class PyGeoAPIServerTests(unittest.TestCase):
             'gcloud', 'storage', 'cp', '--recursive',
             f'{execution_response["workspace_url"]}/*', local_dest_path
         ], check=True)
+
+        with open(os.path.join(local_dest_path, 'stdout.log')) as f:
+            print(f.read())
+
+        with open(os.path.join(local_dest_path, 'stderr.log')) as f:
+            print(f.read())
+
         self.assertEqual(
             set(os.listdir(local_dest_path)),
             {
